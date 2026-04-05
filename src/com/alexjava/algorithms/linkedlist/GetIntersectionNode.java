f6c7e332a10061b54ac9d78de33d7556b9263d02
alex <962213127@qq.com>
2026-04-05 10:26:32 +0800

    add listnode code

---
+++ src/com/alexjava/algorithms/linkedlist/GetIntersectionNode.java
@@ -0,0 +1,23 @@
+package com.alexjava.algorithms.linkedlist;
+
+import com.alexjava.common.structures.ListNode;
+
+public class GetIntersectionNode {
+
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+
+        ListNode p1 = headA, p2 = headB;
+
+        while (p1 != p2){
+            if(p1 == null) p1 = headB;
+            else  p1 = p1.next;
+
+            if(p2 == null) p2 = headA;
+            else  p2 = p2.next;
+        }
+
+        return p1;
+    }
+}
